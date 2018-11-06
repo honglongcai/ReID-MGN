60aa0033bf0a36d49b57c75a7b8e2a8f01317393
honglongcai <honglongcai@gmail.com>
2018-11-06 11:53:54 -0800

    11/06/2018

--- train_eval.py
+++ train_eval.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     loss = Loss()
     reid = Main(model, loss, loader)
     data_dir = opt.data_path.split('/')[-1]
-    os.makedirs('weights/' + data_dir)
+    os.mkdir('weights/' + data_dir)
 
     if opt.mode == 'train':
 
