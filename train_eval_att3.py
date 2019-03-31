8aba93de1fc8d390f70d016161ca652fa0d50d4d
honglongcai <honglongcai@gmail.com>
2019-03-31 13:02:30 -0700

    03/31/2019

--- train_eval_att3.py
+++ train_eval_att3.py
@@ -6,7 +6,7 @@ import torch
 from torch.optim import Adam, lr_scheduler
 
 from opt import opt
-from data_2_ex import Data
+from data_aug import Data
 from network_att2 import MGN
 from loss import Loss
 from functions import mean_ap, cmc, re_ranking
