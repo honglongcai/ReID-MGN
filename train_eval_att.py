39a418f34b7ce51eca63784ad3f82746b657e81d
honglongcai <honglongcai@gmail.com>
2019-03-30 23:08:32 -0700

    03/30/2019

--- train_eval_att.py
+++ train_eval_att.py
@@ -64,7 +64,7 @@ class Main():
         g_g_dist = np.dot(gf, np.transpose(gf))
         dist = re_ranking(q_g_dist, q_q_dist, g_g_dist)
         r = cmc(dist, self.queryset.ids, self.testset.ids, self.queryset.cameras, self.testset.cameras,
-                separate_camera_set=True,
+                separate_camera_set=False,
                 single_gallery_shot=False,
                 first_match_break=True)
         m_ap = mean_ap(dist, self.queryset.ids, self.testset.ids, self.queryset.cameras, self.testset.cameras)
@@ -101,7 +101,7 @@ class Main():
         #########################no re rank##########################
         dist = cdist(qf, gf)
         r = cmc(dist, self.queryset.ids, self.testset.ids, self.queryset.cameras, self.testset.cameras,
-                separate_camera_set=True,
+                separate_camera_set=False,
                 single_gallery_shot=False,
                 first_match_break=True)
         m_ap = mean_ap(dist, self.queryset.ids, self.testset.ids, self.queryset.cameras, self.testset.cameras)
